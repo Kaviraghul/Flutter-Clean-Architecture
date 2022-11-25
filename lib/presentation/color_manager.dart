6f5403eba98210e7ee02658af5442dbd95bdabbe
Kaviraghul <vettrithamil@gmail.com>
2022-11-25 23:45:21 +0530

    added color manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/animation.dart';
+
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = HexColor.fromHex('#ED9728');
+  static Color darkGrey = HexColor.fromHex('#525252');
+  static Color grey = HexColor.fromHex('#737477');
+  static Color lightGrey = HexColor.fromHex('#9E9E9E');
+  static Color primaryOpacity70 = HexColor.fromHex('#B3ED9728');
+}
+
+extension HexColor on Color {
+  static Color fromHex(String hexColorString) {
+    hexColorString = hexColorString.replaceAll('#', '');
+    if (hexColorString.length == 6) {
+      hexColorString = "FF" + hexColorString;
+    }
+
+    return Color(int.parse(hexColorString));
+  }
+}
