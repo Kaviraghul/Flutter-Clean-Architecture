13bde8777ec030e35752033d036ecb30d279bd6f
Kaviraghul <vettrithamil@gmail.com>
2022-11-25 22:19:43 +0530

    created app.dart file

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal(); // private named constructor
+
+  static final MyApp instance = MyApp
+      ._internal(); //singleton for single instance so only one object gets created for both ios and android
+
+  factory MyApp() => instance;
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
