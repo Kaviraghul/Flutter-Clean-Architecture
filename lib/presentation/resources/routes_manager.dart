d9e84ea53fa3929ce14b3a93eab9be582adbab88
Kaviraghul <vettrithamil@gmail.com>
2022-11-26 14:33:03 +0530

    added route generator in routes manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/presentation/splash/splash.dart';
+
 class Routes {
   static const String splashRoute         = "/";
   static const String onBoardingRoute     = "/onBoarding";
@@ -6,4 +10,35 @@ class Routes {
   static const String forgotPasswardRoute = "/forgotPassword";
   static const String mainRoute           = "/main";
   static const String storeDetailsRoute   = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings){
+    switch (routeSettings.name){
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.forgotPasswardRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute(){
+    return MaterialPageRoute(builder: (_) => 
+       Scaffold(
+        appBar: AppBar(title: const Text("No route found")),
+       )
+    );
+  }
 }
