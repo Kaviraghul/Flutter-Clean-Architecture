e876ed48ff81fd4eeb6b832e22e0a6e0092a4355
Kaviraghul <vettrithamil@gmail.com>
2022-11-26 11:23:51 +0530

    added button and elevated button theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -10,17 +10,41 @@ ThemeData getApplicationTheme() {
       primaryColorLight: ColorManager.primaryOpacity70,
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
+
+      //card theme
+
       cardTheme: CardTheme(
           color: ColorManager.white,
           shadowColor: ColorManager.grey,
           elevation: AppSize.s4),
+
+      //app bar theme
+
       appBarTheme: AppBarTheme(
           centerTitle: true,
           color: ColorManager.primary,
           elevation: AppSize.s4,
           shadowColor: ColorManager.primaryOpacity70,
           titleTextStyle: getRegularStyle(
-              color: ColorManager.white, fontSize: FontSize.s16)));
+              color: ColorManager.white, fontSize: FontSize.s16)),
+
+      //button theme
+
+      buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primaryOpacity70,
+      ),
+
+      //elevated button theme
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              textStyle: getRegularStyle(color: ColorManager.white),
+              foregroundColor: ColorManager.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(AppSize.s12)))));
 
   return theme.copyWith(
     colorScheme: theme.colorScheme.copyWith(secondary: ColorManager.grey),
