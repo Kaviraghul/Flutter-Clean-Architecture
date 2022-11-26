de0f90e96a91b23fe6ac6e50763b956cbd040260
Kaviraghul <vettrithamil@gmail.com>
2022-11-26 13:54:49 +0530

    added folders and view files for all the screens

---
+++ lib/presentation/onboarding/onboarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({ Key? key }) : super(key: key);
+
+  @override
+  _OnboardingViewState createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
