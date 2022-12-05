ad0434805a6d357375cf27bd37875f785c5966b9
Kaviraghul <vettrithamil@gmail.com>
2022-12-05 23:57:59 +0530

    added timer in the splash screen

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/gen/assets.gen.dart';
 import 'package:flutter_clean_architecture/presentation/resources/color_manager.dart';
+import 'package:flutter_clean_architecture/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({ Key? key }) : super(key: key);
@@ -10,6 +13,29 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    _startDelay();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     const assets = Assets.images;
