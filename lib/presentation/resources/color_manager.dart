b213b57d252319a2627377978c0d3734519613e2
Kaviraghul <vettrithamil@gmail.com>
2022-12-05 23:38:44 +0530

    initial set up for splash screen

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -3,27 +3,28 @@ import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static Color primary = HexColor.fromHex('#ED9728');
-  static Color darkGrey = HexColor.fromHex('#525252');
-  static Color grey = HexColor.fromHex('#737477');
-  static Color lightGrey = HexColor.fromHex('#9E9E9E');
+  static Color primary          = HexColor.fromHex('#ffffff');
+  static Color darkGrey         = HexColor.fromHex('#525252');
+  static Color grey             = HexColor.fromHex('#737477');
+  static Color lightGrey        = HexColor.fromHex('#9E9E9E');
   static Color primaryOpacity70 = HexColor.fromHex('#B3ED9728');
 
   // new colors
-  static Color darkPrimary = HexColor.fromHex('#d17d11');
-  static Color grey1 = HexColor.fromHex('#707070');
-  static Color grey2 = HexColor.fromHex('#797979');
-  static Color white = HexColor.fromHex('#FFFFFF');
-  static Color error = HexColor.fromHex('#E61F34');
+  static Color darkPrimary      = HexColor.fromHex('#d17d11');
+  static Color grey1            = HexColor.fromHex('#707070');
+  static Color grey2            = HexColor.fromHex('#797979');
+  static Color white            = HexColor.fromHex('#FFFFFF');
+  static Color error            = HexColor.fromHex('#E61F34');
 }
 
 extension HexColor on Color {
   static Color fromHex(String hexColorString) {
     hexColorString = hexColorString.replaceAll('#', '');
     if (hexColorString.length == 6) {
-      hexColorString = "FF$hexColorString";
+      hexColorString = "0xFF$hexColorString";
+    }else if (hexColorString.length == 8) {
+      hexColorString = "0x$hexColorString";
     }
-
     return Color(int.parse(hexColorString));
   }
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/gen/assets.gen.dart';
+import 'package:flutter_clean_architecture/presentation/resources/color_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({ Key? key }) : super(key: key);
@@ -10,8 +12,11 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Hi HRU?")),
+    const assets = Assets.images;
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+
+      body: Center(child: Image.asset(assets.onBoardingImage1.path))
     );
   }
 }
