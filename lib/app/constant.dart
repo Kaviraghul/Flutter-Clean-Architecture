ddbc218cf260a63495520b5b8b9736906a274791
Kaviraghul <vettrithamil@gmail.com>
2022-12-08 13:09:01 +0530

    added app api service in network layer, and refractored the responses file

---
+++ lib/app/constant.dart
@@ -0,0 +1,3 @@
+class Constant{
+  static const String baseUrl = "https://kaviraghul.mocklab.io/";
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,16 @@
+
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/app/constant.dart';
+import 'package:flutter_clean_architecture/data/responses/responses.dart';
+import 'package:retrofit/http.dart';
+
+ part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constant.baseUrl)
+abstract class AppServiceClient{
+
+  factory AppServiceClient(Dio Dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login();
+}
