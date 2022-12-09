0fb2198391181865320df52bef002ce767b71cb9
Kaviraghul <vettrithamil@gmail.com>
2022-12-09 13:43:44 +0530

    added models for contact, customer and authentication

--- lib/data/model.dart
+++
@@ -1,7 +0,0 @@
-class SliderObject {
-  String title;
-  String subTitle;
-  String image;
-
-  SliderObject(this.title, this.subTitle, this.image);
-}
---
+++ lib/domain/model.dart
@@ -0,0 +1,33 @@
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
+}
+
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+
+class Contacts {
+  String email;
+  String phone;
+  String link;
+
+  Contacts(this.email, this.phone, this.link);
+}
+
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.contacts, this.customer);
+}
