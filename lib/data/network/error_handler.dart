70dfed13c0206fd529902ebc234b5e8d7d0487e2
Kaviraghul <vettrithamil@gmail.com>
2022-12-11 21:19:51 +0530

    added data source enum

---
+++ lib/data/network/error_handler.dart
@@ -0,0 +1,31 @@
+// data source enum can contain all posible values of a data source status
+
+enum DataSource {
+  // ignore: constant_identifier_names
+  SUCCESS,
+  // ignore: constant_identifier_names
+  NO_CONTENT,
+  // ignore: constant_identifier_names
+  BAD_REQUEST,
+  // ignore: constant_identifier_names
+  FORBIDDEN,
+  // ignore: constant_identifier_names
+  UNAUTHORISED,
+  // ignore: constant_identifier_names
+  NOT_FOUND,
+  // ignore: constant_identifier_names
+  INTERNAL_SERVER_ERROR,
+  // ignore: constant_identifier_names
+  CONNECT_TIMEOUT,
+  // ignore: constant_identifier_names
+  CANCEL,
+  // ignore: constant_identifier_names
+  RECIEVE_TIMEOUT,
+  // ignore: constant_identifier_names
+  SEND_TIMEOUT,
+  // ignore: constant_identifier_names
+  CACHE_ERROR,
+  // ignore: constant_identifier_names
+  NO_INTERNET_CONNECTION
+  // ignore: constant_identifier_names
+}
