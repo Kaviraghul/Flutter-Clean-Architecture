30876cc3dd1b8a5a080b96faa26075cf980c5c64
Kaviraghul <vettrithamil@gmail.com>
2022-12-11 21:41:01 +0530

    added response code

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -27,5 +27,43 @@ enum DataSource {
   CACHE_ERROR,
   // ignore: constant_identifier_names
   NO_INTERNET_CONNECTION
+}
+
+class ResponseCode{
+
+  // API status code
+
   // ignore: constant_identifier_names
+  static const int SUCCESS                = 200;  // success with data
+  // ignore: constant_identifier_names
+  static const int NO_CONTENT             = 201;  // success with no content
+  // ignore: constant_identifier_names
+  static const int BAD_REQUEST            = 400;  // failure, api rejected the request
+  // ignore: constant_identifier_names
+  static const int FORBIDDEN              = 403;  // failure, api rejected the request
+  // ignore: constant_identifier_names
+  static const int UNAUTHORISED           = 401;  // failure, user is not authorised
+  // ignore: constant_identifier_names
+  static const int NOT_FOUND              = 404;  // failure, api url is not correct and not found
+  // ignore: constant_identifier_names
+  static const int INTERNAL_SERVER_ERROR  = 500;  // failure crash happened in server side
+
+
+  // Local status code  for network layer and interlogics
+
+  // ignore: constant_identifier_names
+  static const int UNKNOWN                = -1;
+  // ignore: constant_identifier_names
+  static const int CONNECT_TIMEOUT        = -2;
+  // ignore: constant_identifier_names
+  static const int CANCEL                 = -3;
+  // ignore: constant_identifier_names
+  static const int RECIEVE_TIMEOUT        = -4;
+  // ignore: constant_identifier_names
+  static const int SEND_TIMEOUT           = -5;
+  // ignore: constant_identifier_names
+  static const int CACHE_ERROR            = -6;
+  // ignore: constant_identifier_names
+  static const int NO_INTERNET_CONNECTION = -7;
+
 }
