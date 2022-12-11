3455cf6071e127e6bf52cdd5989602e613620057
Kaviraghul <vettrithamil@gmail.com>
2022-12-11 21:49:54 +0530

    added response message

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -67,3 +67,43 @@ class ResponseCode{
   static const int NO_INTERNET_CONNECTION = -7;
 
 }
+
+
+class ResponseMessage{
+
+  // API status code
+
+  // ignore: constant_identifier_names
+  static const String SUCCESS                = "success";  // success with data
+  // ignore: constant_identifier_names
+  static const String NO_CONTENT             = "success with no content";  // success with no content
+  // ignore: constant_identifier_names
+  static const String BAD_REQUEST            = "Bad request, try again later";  // failure, api rejected the request
+  // ignore: constant_identifier_names
+  static const String FORBIDDEN              = "forbidden request, try again later";  // failure, api rejected the request
+  // ignore: constant_identifier_names
+  static const String UNAUTHORISED           = "user is unauthorised, try again later";  // failure, user is not authorised
+  // ignore: constant_identifier_names
+  static const String NOT_FOUND              = "Url is not found, try again later";  // failure, api url is not correct and not found
+  // ignore: constant_identifier_names
+  static const String INTERNAL_SERVER_ERROR  = "something went wrong, try again later";  // failure crash happened in server side
+
+
+  // Local status code  for network layer and interlogics
+
+  // ignore: constant_identifier_names
+  static const String UNKNOWN                = "something went wrong, try again later";
+  // ignore: constant_identifier_names
+  static const String CONNECT_TIMEOUT        = "time out error, try again later";
+  // ignore: constant_identifier_names
+  static const String CANCEL                 = "request was cancelled, try again later";
+  // ignore: constant_identifier_names
+  static const String RECIEVE_TIMEOUT        = "time out error, try again later";
+  // ignore: constant_identifier_names
+  static const String SEND_TIMEOUT           = "time out error, try again later";
+  // ignore: constant_identifier_names
+  static const String CACHE_ERROR            = "Cache error, try again later";
+  // ignore: constant_identifier_names
+  static const String NO_INTERNET_CONNECTION = "Please check your internet connection";
+
+}
