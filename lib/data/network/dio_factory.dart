9346a4daebf6c03e888759575058d5bbd247d43a
Kaviraghul <vettrithamil@gmail.com>
2022-12-17 09:40:52 +0530

    added pretty dio logger to log all the request, responce

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_clean_architecture/app/constant.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 
 // ignore: constant_identifier_names
@@ -35,6 +37,19 @@ class DioFactory{
       receiveTimeout:_timeOut,
       headers: headers,
     );
+
+
+    // this logger must only pring when it is in deug mode not in release mode
+    if(kReleaseMode){
+      // ignore: avoid_print
+      print("release mode on logs");
+    }else{
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+      ));
+    }
 
     return dio;
   }
