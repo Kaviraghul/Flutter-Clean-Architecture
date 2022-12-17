b4d8cd19f7bb286555d571c0007e5a1695837c8a
Kaviraghul <vettrithamil@gmail.com>
2022-12-17 16:59:08 +0530

    added device info and completed clean architecture module

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -31,3 +31,11 @@ class Authentication {
 
   Authentication( this.customer, this.contacts);
 }
+
+
+class DeviceInfo{
+  String name;
+  String identifier;
+  String version;
+  DeviceInfo(this.name, this.identifier, this.version);
+}
