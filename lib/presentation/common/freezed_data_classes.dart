459c6e661371a92be43c248b37263a314e1cbec5
Kaviraghul <vettrithamil@gmail.com>
2023-01-06 14:37:52 +0530

    added freezed package for login object

---
+++ lib/presentation/common/freezed_data_classes.dart
@@ -0,0 +1,10 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'freezed_data_classes.freezed.dart';
+
+@freezed
+class LoginObject with _$LoginObject{
+  factory LoginObject({required String username, required String password}) = _LoginObject;
+}
--- lib/presentation/login/loginViewModel.dart
+++ lib/presentation/login/loginViewModel.dart
@@ -1,18 +1,20 @@
 import 'dart:async';
 
 import 'package:flutter_clean_architecture/presentation/base/base_view_model.dart';
+import 'package:flutter_clean_architecture/presentation/common/freezed_data_classes.dart';
 
 class LoginViewModel extends BaseViewModel with LoginViewModelInput,LoginViewModelOutput{
 
 //two streem controllers for user name and user password 
-  StreamController UserNameStreamController = StreamController<String>.broadcast();
-  StreamController PasswordStreamController = StreamController<String>.broadcast();
+  StreamController userNameStreamController = StreamController<String>.broadcast();
+  StreamController passwordStreamController = StreamController<String>.broadcast();
 
+  var loginObject = LoginObject(username: "", password: "");
 
   @override
   void dispose() {
-    UserNameStreamController.close();
-    PasswordStreamController.close();
+    userNameStreamController.close();
+    passwordStreamController.close();
   }
 
   @override
@@ -27,28 +29,28 @@ class LoginViewModel extends BaseViewModel with LoginViewModelInput,LoginViewMod
   }
   
   @override
-  setPassword() {
-    // TODO: implement setPassword
-    throw UnimplementedError();
+  setPassword(String password) {
+    inputPassword.add(password);
+    loginObject = loginObject.copyWith(password: password);
   }
   
   @override
-  setUsername() {
-    // TODO: implement setUsername
-    throw UnimplementedError();
+  setUsername(String username) {
+    inputUsername.add(username);
+    loginObject = loginObject.copyWith(username: username);
   }
 
   @override
-  Sink get inputPassword => PasswordStreamController.sink;
+  Sink get inputPassword => passwordStreamController.sink;
   
   @override
-  Sink get inputUsername => UserNameStreamController.sink;
+  Sink get inputUsername => userNameStreamController.sink;
 
   @override
-  Stream<bool> get outputLoginPassword => PasswordStreamController.stream.map((password) => _isPasswordValid(password));
+  Stream<bool> get outputLoginPassword => passwordStreamController.stream.map((password) => _isPasswordValid(password));
   
   @override
-  Stream<bool> get outputLoginUsername => UserNameStreamController.stream.map((username) => _isUsernameValid(username));
+  Stream<bool> get outputLoginUsername => userNameStreamController.stream.map((username) => _isUsernameValid(username));
 
   bool _isPasswordValid(String password){
     return password.isNotEmpty;
@@ -60,8 +62,8 @@ class LoginViewModel extends BaseViewModel with LoginViewModelInput,LoginViewMod
 }
 
 abstract class LoginViewModelInput{
-  setUsername();
-  setPassword();
+  setUsername(String username);
+  setPassword(String password);
   login();
   Sink get inputUsername;
   Sink get inputPassword;
